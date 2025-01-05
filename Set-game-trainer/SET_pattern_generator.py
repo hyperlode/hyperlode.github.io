b23d2e1801fe0e7a324c178c3aa5bbe040340026
opensoapstand <opensoapstand@gmail.com>
2025-01-04 16:05:16 -0800

    Store and retrieve from db

--- Set-game-trainer/SET_pattern_generator.py
+++ Set-game-trainer/SET_pattern_generator.py
@@ -4,12 +4,21 @@ import itertools
 import random
 from pathlib import Path
 
+import sqlite3
+import json
+
+# set pattern properties
+PATTERN_ROWS = 9
+PATTERN_COLS = 9
+PATTERN_WINDOW_ROWS = 3
+PATTERN_WINDOW_COLS = 4
 
 # Set
 #card options
 PROPERTIES_COUNT = 3
 VALUES_COUNT = 4
-parameters = { "colour":["Red", "Green", "Blue"], "shape":["Squigle","Pill","Diamond"],"quantity":["1","2","3"], "infill":["Solid","Blank","Arced"]}
+parameters = { "quantity":["1","2","3"],"colour":["Red", "Green", "Blue"], "infill":["open","hatched","solid"], "shape":["Squiggle","Pill","Diamond"]}
+parameters_single_char = {"quantity":["1","2","3"], "colour":["R", "G", "B"], "infill":["o","h","s"], "shape":["S","P","D"]}
 
 def create_deck(parameters, formatted=False, shuffled=True):
     # provide dict with all parameters and their values. 
@@ -44,7 +53,7 @@ def is_set(three_cards, verbose=False):
     # check for all values diff or all equal per parameter SET fulfillment requirement
     for diff_values in separated_per_property_as_different_values:
         diff_count = len(diff_values)
-        if len(diff_values) == 1 or len(diff_values) == PROPERTIES_COUNT:
+        if diff_count == 1 or diff_count == PROPERTIES_COUNT:
             pass
         else:
             is_set = False
@@ -89,37 +98,136 @@ def set_in_cards(cards, verbose=False, count_sets=False):
     else:
         return sets_count
 
-PATTERN_COLS = 9
-PATTERN_ROWS = 9
 
-WINDOW_ROWS = 3
-WINDOW_COLS = 4
 
+
+class db_SET_analytics():
+    def __init__(self, db_path):
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+        self.check_integrity()
+        
+    def execute_sql(self, sql_str):
+        self.cursor.execute(sql_str)
+        self.conn.commit()
+        
+    def check_integrity(self):
+        # Function to check if a table exists
+        if not (self.table_exists("set_patterns")):
+            # print("DB not OK. Uncomment next lines to create table. ")
+            self.create_table_set_patterns()
+    
+        
+    def add_pattern(self, set_pattern_dict):
+        # Function to store a list in the database
+        pattern = set_pattern_dict["pattern"]
+        
+        window_stats = set_pattern_dict["window_stats"]
+        single_set_windows = window_stats[1]
+        
+        # Convert the list to JSON
+        json_pattern = json.dumps(pattern)
+        json_window_stats = json.dumps(window_stats)
+        
+        self.execute_sql(
+            "INSERT INTO set_patterns (pattern, window_stats, single_set_windows) VALUES ('{}','{}','{}')".format(
+                json_pattern, json_window_stats, single_set_windows
+                ))
+        
+    def table_exists(self, table_name):
+        self.cursor.execute("""
+            SELECT name 
+            FROM sqlite_master 
+            WHERE type='table' AND name=?;
+        """, (table_name,))
+        return self.cursor.fetchone() is not None
+    
+    def create_table_set_patterns(self):
+        # Create a table
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS set_patterns (
+            pattern TEXT PRIMARY KEY,
+            window_stats TEXT,
+            single_set_windows INTEGER
+        )
+        """)
+    
+    def get_pattern_with_most_single_set_windows(self):
+        self.cursor.execute(f"""
+            SELECT * FROM set_patterns
+            ORDER BY single_set_windows DESC
+            LIMIT 1
+            """)
+        
+        row = self.cursor.fetchone()
+        pattern_json, window_stats_json, single_set_windows = row
+
+        pattern = json.loads(pattern_json)
+        window_stats_json = json.loads(window_stats_json)
+        single_set_windows = int(single_set_windows)
+        return {"pattern":pattern, "window_stats_json":window_stats_json, "single_set_windows":single_set_windows}
 
                               
 class SET(): 
     
     def __init__(self):
+       self.reset_pattern(); 
         
-        
+    def reset_pattern(self):
         #self.base_pattern_positions = self.get_all_basic_pattern_positions()
         
         # normal pattern: 9x9. But, to handle the edge cases: we extend the 9x9 pattern 2x2  so: 18x18. where (0,0) == (9,0) == (0,9) == (9,9)
-        self.pattern_extended = {(row,col):None for col in range(PATTERN_COLS*2) for row in range(PATTERN_ROWS *2)}
+        self.pattern_extended = {(row,col):None  for row in range(PATTERN_ROWS *2) for col in range(PATTERN_COLS*2)}
         
-        # top left corner of a cards window in the pattern.  
-        self.set_counts_pattern = {(row,col):0 for col in range(PATTERN_COLS) for row in range(PATTERN_ROWS)}
+        # top left corner of a cards window in the pattern. every position contains the amount of sets in the window with position as upper left corner of this window.
+        self.set_counts_pattern = {(row,col):0 for row in range(PATTERN_ROWS) for col in range(PATTERN_COLS) }
 
         self.pattern_total_sets_count = None
         self.sets_count_window_distribution= None
+        
+    def card_compact_to_normal(self, compact_card):
+       
+        #  'Squiggle'), (15, 17): ('2', 'Blue', 'solid', 'Squiggle'), (16, 17): ('2', 'Blue', 'hatched', 'Diamond'), (17, 17): ('3', 'Blue', 'open', 'Pill')}
+         
+        card = []
+        # properties = list(parameters_single_char.keys())
+        # print(compact_card)
+        for property_index, value in enumerate(compact_card):
+            # print(property_index)
+            # print(value)
+            # print(parameters.keys())
+            property= list(parameters.keys())[property_index]
+            # print(parameters[property])
+            value_index = parameters_single_char[property].index(value)
+            
+            # card[property] = parameters[property][value_index]
+            card.append(parameters[property][value_index])
+        card = tuple(card)
+        return card
+        
+    def restore_archived_pattern(self, pattern_dict):
+        # pattern_dict --> contains "pattern" key with compacted pattern value. e.g. ['2RoP', '2RsP', '2GoS', '2RhP', '1RhD', '2RsD', '2GoD', '2GoP', '3RoS', '1RsD', '3BhD', '3BsS', '1BoP', '2RsS', '2BoP', '3RoP', '2GhP', '1RoD', '3BoD', '1RhP', '1RsP', '1BoD', '2BoS', '2BhS', '2GhS', '3RhD', '1RoP', '1GoS', '3GsP', '2BsD', '3RsS', '2GhD', '1RoS', '3BsD', '1BsD', '3GoP', '1BhS', '3BoP', '1RhS', '2BsS', '2GsS', '1GhS', '2BsP', '2BoD', '2GsD', '1GsP', '3GhS', '2RhD', '1GsD', '1GoP', '2BhP', '1GhD', '1BoS', '3GsD', '1GsS', '3GoD', '1GhP', '2BhD', '3GhP', '2GsP', '2RoS', '3GsS', '3GoS', '1BsS', '3BoS', '1GoD', '1RsS', '3GhD', '2RhS', '1BhD', '3RoD', '1BhP', '2RoD', '1BsP', '3BhS', '3RsD', '3RhS', '3RhP', '3BhP', '3BsP', '3RsP']
+        self.reset_pattern()
+        compact_pattern = pattern_dict["pattern"]
+            
+        positions = self.get_all_basic_pattern_positions()
+        for card_str,position in zip(compact_pattern,positions):
+            print("origin card string and position: {} , {}".format(card_str,position))
+            card_tuple = self.card_compact_to_normal(card_str)
+            self.add_card_to_pattern(card_tuple, position)
+        
+        self.print_pattern()
+        
+    def get_pattern_as_dict(self):
+        return {"pattern":self.get_pattern_compact(False, True, True), "window_stats":self.sets_count_window_distribution}
          
     def get_all_basic_pattern_positions(self):
-        return [(row, col) for col in range(PATTERN_COLS) for row in range(PATTERN_ROWS) ]
+        return [(row, col) for row in range(PATTERN_ROWS) for col in range(PATTERN_COLS)  ]
       
     def add_card_to_pattern(self, card, position):
         
-        initial_col = position[0]
-        initial_row = position[1]
+        initial_row = position[0]
+        initial_col = position[1]
         
         extended_col = initial_col
         extended_row = initial_row
@@ -133,14 +241,38 @@ class SET():
         else:
             extended_row -= PATTERN_ROWS
             
-        self.pattern_extended[(initial_col, initial_row)] = card
-        self.pattern_extended[(initial_col, extended_row)] = card
-        self.pattern_extended[(extended_col, initial_row)] = card
-        self.pattern_extended[(extended_col, extended_row)] = card
+        self.pattern_extended[(initial_row, initial_col)] = card
+        self.pattern_extended[(initial_row, extended_col)] = card
+        self.pattern_extended[(extended_row, initial_col)] = card
+        self.pattern_extended[(extended_row, extended_col)] = card
     
     def get_card_from_pattern(self, position):
         return self.pattern_extended[position]
     
+    def get_pattern_compact(self, extended=False, compacted=True, as_list=True):
+        # if extended:
+        #     AssertionError, "not implemented"
+        return_pattern = {}
+        for row in range(PATTERN_ROWS + extended * PATTERN_ROWS):
+            for col in range(PATTERN_COLS + extended * PATTERN_COLS):
+                return_pattern[(row,col)] = self.pattern_extended[(row,col)]
+                
+        compacted_dict = {}
+        if compacted:
+            for k,v in return_pattern.items():
+                compacted_dict[k] = "".join(item[0] for item in v)  # take first char from each value
+            return_pattern = compacted_dict
+        
+        pattern_as_list =[]
+        if as_list:
+            for row in range(PATTERN_ROWS + extended * PATTERN_ROWS):
+                for col in range(PATTERN_COLS + extended * PATTERN_COLS):
+                    val = return_pattern[(row,col)]
+                    pattern_as_list.append(val)
+            return_pattern = pattern_as_list
+        
+        return return_pattern
+        
     def get_pattern_as_string(self, print_extended=False):
         pattern_str = ""
         for row in range(PATTERN_ROWS + print_extended * PATTERN_ROWS):
@@ -148,9 +280,9 @@ class SET():
             for col in range(PATTERN_COLS + print_extended * PATTERN_COLS):
                 card_tuple = self.pattern_extended[(row,col)]
                 if card_tuple is None:
-                    card_tuple_short = "xxxxxxx"
+                    card_tuple_short = "xxxx"
                 else:    
-                    card_tuple_short = [t[:2] for t in card_tuple]
+                    card_tuple_short = [t[:1] for t in card_tuple]
                     
                 card_string = ""
                 for element in card_tuple_short:
@@ -165,7 +297,7 @@ class SET():
         print(self.get_pattern_as_string(print_extended))
     
     def get_pattern_positions_from_window_position(self, window_position):
-        return [ (window_position[0] + row ,window_position[1] + col) for row in range (WINDOW_ROWS) for col in range(WINDOW_COLS) ]
+        return [ (window_position[0] + row ,window_position[1] + col) for row in range (PATTERN_WINDOW_ROWS) for col in range(PATTERN_WINDOW_COLS) ]
         
     def get_window_cards_from_pattern(self, window_position ):
         positions = self.get_pattern_positions_from_window_position(window_position)
@@ -201,7 +333,7 @@ class SET():
     def print_set_counts_pattern(self):
             print(self.get_set_counts_pattern_as_string())
             
-    def pattern_stats(self):
+    def pattern_stats(self, verbose=False):
         sets_count = 0
         sets_count_window_distribution= [0 for i in range(20)]
         
@@ -210,12 +342,14 @@ class SET():
                 set_count = self.set_counts_pattern[(row,col)]
                 sets_count += set_count
                 sets_count_window_distribution[set_count]+=1
-        print("Total sets: {}" .format(sets_count))
-        print("Sets per window distribution: {}" .format(sets_count_window_distribution))
+        if verbose:
+            print("Total sets: {}" .format(sets_count))
+            print("Sets per window distribution: {}" .format(sets_count_window_distribution))
         check_count = 0
         for i,count in enumerate(sets_count_window_distribution):
             check_count += i*count
-        print("check: {}".format(check_count))
+        if verbose:
+            print("check: {}".format(check_count))
         self.pattern_total_sets_count = sets_count
         self.sets_count_window_distribution= sets_count_window_distribution 
         return sets_count_window_distribution
@@ -230,9 +364,10 @@ class SET():
             file.write(f"{pattern_string}\n")
             sets_counts_windows_as_string = self.get_set_counts_pattern_as_string()
             file.write(f"{sets_counts_windows_as_string}\n")
-                    
-if __name__ == "__main__":
-    for i in range (10000):
+               
+               
+def generate_set_patterns_to_file():
+    for i in range (1):
         deck = create_deck(parameters, False, True)
         setgame = SET()
         positions = setgame.get_all_basic_pattern_positions()
@@ -245,4 +380,42 @@ if __name__ == "__main__":
         setgame.pattern_stats()
         setgame.save_pattern_to_file("C:\Data\generated_program_data\SET_pattern")
         
+    
+def generate_set_patterns_to_db(db_path,attempts=100):
+     
+    db_set = db_SET_analytics(db_path)
+    
+    for i in range (attempts):
+        deck = create_deck(parameters, False, True)
+        setgame = SET()
+        positions = setgame.get_all_basic_pattern_positions()
+        for pos in positions:
+            setgame.add_card_to_pattern(deck.pop(), pos)
+        setgame.calculate_all_windows_sets_count()
+        # setgame.print_pattern(False)
+        # print(setgame.pattern_extended)
+        # setgame.print_set_counts_pattern()
+        # window_positions = setgame.get_pattern_positions_from_window_position((0,0))
+        setgame.pattern_stats()
+        onepattern_dict = setgame.get_pattern_as_dict()
+        
+        db_set.add_pattern(onepattern_dict)
+        
+def retrieve_most_promising_pattern(db_path):
+    db_set = db_SET_analytics(db_path)
+    pattern_dict = db_set.get_pattern_with_most_single_set_windows()
+    setgame = SET()
+    print(pattern_dict)
+    setgame.restore_archived_pattern(pattern_dict)
+    setgame.print_pattern()
+    setgame.calculate_all_windows_sets_count()
+    setgame.pattern_stats(True)
+    setgame.print_set_counts_pattern()
+    
+if __name__ == "__main__":
     # print(window_positions)
+    db_path = "C:\Data\generated_program_data\SET_pattern_searcher\set_patterns.db"
+    # generate_set_patterns_to_db(db_path, attempts=1)
+    retrieve_most_promising_pattern(db_path)
+    # setgame = SET()
+    # setgame.card_compact_to_normal("2GoD")    
